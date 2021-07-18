160b5dc98cc637f7df2f2869bb238ea955409e9e
ottomattas <otto.mattas@gmail.com>
2021-07-19 00:28:42 +0200

    Finish the agent

--- 1_bandit-agent/bandit_agent.py
+++ 1_bandit-agent/bandit_agent.py
@@ -4,124 +4,126 @@
 import game as g
 from random_agent import RandomAgent
 
-import random, copy, time
+import random
+import copy
+import time
 import numpy as np
-import sys
-sys.setrecursionlimit(100000)
 
-class BanditAgent:
+class BanditAgent():
     def __init__(self, timelimit, id):
         self.timelimit = timelimit
         self.id = id
 
     def make_move(self, g):
         start = time.perf_counter()
-        epsilon = 0
+        start_epsilon = 1.0
+        end_epsilon = 0.0
+        number_of_possible_moves = len(g.board.free_positions())
         
+        # Create array for possible epsilon values
+        epsilon_array = np.linspace(start_epsilon, end_epsilon, self.timelimit)
+        #print("Epsilon decay list:", epsilon_array)
+
+        # Create arrays for evaluating the position
+        position_win_probability = np.zeros(number_of_possible_moves)
+        position_score = np.zeros(number_of_possible_moves)
+        position_counter = np.zeros(number_of_possible_moves)
+
         # run until time is up
         while time.perf_counter() - start < self.timelimit / 1000:
-            # print(epsilon)
-            # free_positions = g.board.random_free()
-            # print(free_positions)
-            # break
-
-            # start rollout
             
-            # if epsilon < random.value, then explore
-            if random.random() > epsilon:
-                # Copy the board for simulation
-                b = copy.deepcopy(g.board)
-                print("Deepcopy of the board is \n", b)
+            # Check for free positions
+            free_positions = g.board.free_positions()
+            #print("Free positions:\n", free_positions)
 
-                # Check the free positions
-                free_positions = b.free_positions()
-                print("Free positions are \n", free_positions)
+            #################
+            # Start rollout #
+            #################
 
-                # Choose a random next action
-                next_move = random.choice(free_positions)
-                print("Random next_move is", next_move)
+            # Copy the board for simulation
+            b = copy.deepcopy(g.board)
+            #print("Deepcopy:\n", b)
 
-                # Take the action on the copied board
-                b.place(next_move, 1)
-                print("Action has been taken as the board shows \n", b)
+            # Create an index for the move
+            index = random.randrange(0, len(free_positions))
 
-                # Set parameters for the simulation game
-                # Use two random agents and the deepcopy of the board
-                simulation_players = [RandomAgent(1), RandomAgent(2)]
-                g.from_board(b, g.objectives, simulation_players, g.print_board)
-                print("The return of the from_board is \n", b, g.objectives, simulation_players, g.print_board)
+            # Set epsilon
+            epsilon = epsilon_array[0]
+            print("Epsilon: ", epsilon)
 
-                # Run the game, player who won is returned
-                player = g.play()
-                print("Returned player from game:", player.id)
+            if epsilon != 0:
+                # Remove the used epsilon for decay
+                epsilon_index = np.where(epsilon_array == epsilon)[0][0]
+                epsilon_array = np.delete(epsilon_array, epsilon_index)
+                #print("Epsilon array after last delete: ", epsilon_array)
                 
-                    # if player 1 , add position_value +1
-                    #if g.player.id == 1:
-                        #position_value =+ 1
+            # Select a greedy move
+            if random.random() > epsilon:
+                # Create a tuple to find the list of highest probabilities
+                highest_probabilities = np.where(position_win_probability == np.amax(position_win_probability))
+                #print('Returned tuple of highest probabilities :', highest_probabilities)
+                
+                # Save the list of indices for those highest probabilities
+                highest_probabilities_indices = highest_probabilities[0]
+                #print('List of Indices of highest probabilities :', highest_probabilities_indices)
+                
+                # Extract the first index for the highest probabilities from
+                # the list as there might be more than one highest probability
+                highest_probability_index = highest_probabilities_indices[0]
+                #print("Highest probability index is", highest_probability_index)
+                
+                # Select the move
+                selected_move = free_positions[highest_probability_index]
+                print("Selected greedy move:", selected_move)
 
-                    # if player 2, add position_value 0
-                    #elif g.player.id == 2:
-                        #position_value =+ 0
+            # Select a random move
+            else:
+                # Select a random move
+                selected_move = free_positions[index]
+                print("Selected random move:", selected_move)
 
-                    # if draw, add position_value 0.5
-                    #else:
-                        #position_value =+ 0,5
-                    # position_counter=+ #how many times was this position chosen
+            # Take the move on the copied board
+            b.place(selected_move, self.id)
 
-                    #position_counter =+ 1
+            # Set parameters for simulation
+            simulation_players = [RandomAgent(1), RandomAgent(2)]
+            simulation_game = g.from_board(b, g.objectives, simulation_players, g.print_board)
 
-            # calculate the average position_value / position_counter, then update the array of chances of winning for the board                
-
-            # if epsilon => random.value, then exploit
-            if random.random() <= epsilon:
-                # Copy the board for simulation
-                b = copy.deepcopy(g.board)
-                print("Deepcopy of the board is", b)
-
-                # Check the free positions
-                free_positions = g.board.free_positions()
-                print("Free positions are", free_positions)
-
-                # Choose a random next action
-                start_position = random.choice(free_positions)
-                print("Random start position is", start_position)
-
-                # Take the action on the copied board
-                b.place(start_position, 1)
-                print("Action has been taken as the board shows:", b)
-
-                # create a new game instance (play) using two random agents and the deepcopied board (from_board)
-                g.from_board(b, g.objectives, g.players, g.print_board)
-                # print("Instantiate a new board for play: ", b)
-                # print("Instantiate objectives for play: ", g.objectives)
-                # print("Instantiate players for play: ", g.players)
-                # print("Instantiate print_board for play: ", g.print_board)
-
-                # game finishes, player who won is returned
-                g.play()
-                print("Returned player from game:", player)
-                break
-                    # look at possible actions, choose best one (randomly if equal bests)
-
-                            # do the move on the deepcopy of the board
-                                # create a new game instance (play) using two random agents and the deepcopied board (from_board)
-                                
-                                # game finishes, player who won
-
-                                # if player 1 , add position_value +1
-
-                                # if player 2, add position_value 0
-
-                                # if draw, add position_value 0.5
-
-                                # position_counter=+ #how many times was this position chosen
-
-                    # calculate the average position_value / position_counter, then update the array of chances of winning for the board
+            # Simulate and determine the winner
+            winner = simulation_game.play()
+            #print("Simulation END:\n", b, "\nWinner is: ", winner)
             
-                # when time passes, grow epsilon. curve vs linear
+            # Give values to states
+            # winner==True
+            if winner:
+                # if player 1 , value 1
+                if winner.id == 1:
+                    position_score[index] += 1
+                    #print("position_score after win", position_score[index])
+
+                # if player 2, value -1
+                else:
+                    position_score[index] -= 1
+                    #print("position_score after loss", position_score[index])
+            # winner==False
+            else:
+                # if draw, value 0
+                position_score[index] += 0
+                #print("position_score after draw", position_score[index])
+
+            # Count the times a position is visited
+            position_counter[index] += 1
+            #print("position_counter is", position_counter[index])
+
+            # Calculate average position value
+            position_win_probability[index] = position_score[index] / position_counter[index]
+            #print("Average value is", position_win_probability[index])
 
         # return the best move you've found here
-        return g.board.random_free()
+        #print("GIVE THE MOVE BACK TO THE ACTUAL GAME:", selected_move)
+        #print("probability:", selected_move)
+        return selected_move
+        #return g.board.random_free()
 
     def __str__(self):
         return f'Player {self.id} (BanditAgent)'
